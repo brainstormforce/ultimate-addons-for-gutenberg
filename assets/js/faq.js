38065284c121f7443d52c453426988f7780ab22a
Raj <rajkiranb@bsf.io>
2020-05-15 16:56:09 +0530

    resolved the open close issue for grid

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -6,7 +6,7 @@
 
 			$(".uagb-faq__wrap").find('.uagb-faq-child__outer-wrap:first-child').find('.uagb-faq-item').addClass('uagb-faq-item-active');
 			
-			$( document ).delegate( ".uagb-faq-questions-button", "click", UAGBFaq.faqButtonClick )
+			$( document ).delegate( ".uagb-faq-layout-accordion .uagb-faq-questions-button", "click", UAGBFaq.faqButtonClick )
 		},
 		/**
 		 * Smooth Scroll.
