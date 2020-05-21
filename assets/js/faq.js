3043506fc2e6babc085a4ec629bd81166bbc3f0a
Raj <rajkiranb@bsf.io>
2020-05-21 15:30:45 +0530

    Converted jquery to JS

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -1,37 +1,141 @@
-( function( $ ) {
+window.addEventListener(
+    'load', function () {
 
-	UAGBFaq = {
+        init();
+    }
+);
 
-		init: function() {
+function init() {
 
-			$(".uagb-faq-expand-first-yes").find('.uagb-faq-child__outer-wrap:first-child').find('.uagb-faq-item').addClass('uagb-faq-item-active');
+	onLoadActions();
 
-			$(".uagb-faq-inactive-other-no").find('.uagb-faq-child__outer-wrap').find('.uagb-faq-item').addClass('uagb-faq-item-active');
-			
-			$( document ).delegate( ".uagb-faq-layout-accordion .uagb-faq-questions-button", "click", UAGBFaq.faqButtonClick )
-		},
-		/**
-		 * Smooth Scroll.
-		 */
-		faqButtonClick: function( e ) {
+	accordionElements = document.getElementsByClassName( 'uagb-faq-layout-accordion' );
+	for ( item of accordionElements ) {
+		questionButtons = item.querySelectorAll( '.uagb-faq-questions-button' );
 
-            var faqItem = $( this ).parent();
-            
-            if ( faqItem.hasClass('uagb-faq-item-active') ) {
-
-                faqItem.removeClass('uagb-faq-item-active');
-                faqItem.find('.uagb-content').slideUp( 'normal');
-            } else {
-
-                faqItem.addClass('uagb-faq-item-active');
-                faqItem.find('.uagb-content').slideDown( 'normal');
-            }
-		},
+		for ( button of questionButtons ) {
+			button.addEventListener("click", faqButtonClick);
+		}
 	}
+}
 
-	$( document ).ready(function() {
-		UAGBFaq.init()
-	})
+function onLoadActions() {
+	
+	var expandFirstelements = document.getElementsByClassName( 'uagb-faq-expand-first-yes' );
+	var inactiveOtherelements = document.getElementsByClassName( 'uagb-faq-inactive-other-no' );
+	
+	for ( item of expandFirstelements ) { 
+		item.querySelectorAll( '.uagb-faq-child__outer-wrap' )[0].getElementsByClassName( 'uagb-faq-item' )[0].classList.add( 'uagb-faq-item-active' );
+	}  
+	
+	for ( item of inactiveOtherelements ) { 
+		otherItems = item.querySelectorAll( '.uagb-faq-child__outer-wrap' );
+		
+		for ( childItem of otherItems ) {
+			childItem.getElementsByClassName( 'uagb-faq-item' )[0].classList.add( 'uagb-faq-item-active' );
+		}
+	}  
+}
+
+function faqButtonClick() {
+
+	var faqItem = this.parentElement;
+	
+	if ( faqItem.classList.contains('uagb-faq-item-active') ) {
+
+		faqItem.classList.remove('uagb-faq-item-active');
+		slideUp( faqItem.getElementsByClassName( 'uagb-content' )[0] );
+	} else {
+
+		faqItem.classList.add('uagb-faq-item-active');
+		slideDown( faqItem.getElementsByClassName( 'uagb-content' )[0] );
+	}
+}
+
+function slideUp(el) {
+	var elem = el;
+	elem.classList.add('slider');
+	elem.classList.add('closed');
+  }
+  function slideDown(el) {
+	var elem = el;
+	elem.classList.add('slider');
+	elem.classList.remove('closed');
+	elem.style.display = "block";
+  }
 
 
-} )( jQuery )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ( function( $ ) {
+
+// 	UAGBFaq = {
+
+// 		init: function() {
+
+// 			$(".uagb-faq-expand-first-yes").find('.uagb-faq-child__outer-wrap:first-child').find('.uagb-faq-item').addClass('uagb-faq-item-active');
+
+// 			$(".uagb-faq-inactive-other-no").find('.uagb-faq-child__outer-wrap').find('.uagb-faq-item').addClass('uagb-faq-item-active');
+			
+// 			$( document ).delegate( ".uagb-faq-layout-accordion .uagb-faq-questions-button", "click", UAGBFaq.faqButtonClick )
+// 		},
+// 		/**
+// 		 * Smooth Scroll.
+// 		 */
+// 		faqButtonClick: function( e ) {
+
+//             var faqItem = $( this ).parent();
+            
+//             if ( faqItem.hasClass('uagb-faq-item-active') ) {
+
+//                 faqItem.removeClass('uagb-faq-item-active');
+//                 faqItem.find('.uagb-content').slideUp( 'normal');
+//             } else {
+
+//                 faqItem.addClass('uagb-faq-item-active');
+//                 faqItem.find('.uagb-content').slideDown( 'normal');
+//             }
+// 		},
+// 	}
+
+// 	$( document ).ready(function() {
+// 		UAGBFaq.init()
+// 	})
+
+
+// } )( jQuery ) 
