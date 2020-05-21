6162edc6d832a5606c3050ca0237228984e98d4f
Raj <rajkiranb@bsf.io>
2020-05-21 15:31:56 +0530

    removed unwanted code

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -56,86 +56,10 @@ function slideUp(el) {
 	var elem = el;
 	elem.classList.add('slider');
 	elem.classList.add('closed');
-  }
-  function slideDown(el) {
+}
+function slideDown(el) {
 	var elem = el;
 	elem.classList.add('slider');
 	elem.classList.remove('closed');
 	elem.style.display = "block";
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ( function( $ ) {
-
-// 	UAGBFaq = {
-
-// 		init: function() {
-
-// 			$(".uagb-faq-expand-first-yes").find('.uagb-faq-child__outer-wrap:first-child').find('.uagb-faq-item').addClass('uagb-faq-item-active');
-
-// 			$(".uagb-faq-inactive-other-no").find('.uagb-faq-child__outer-wrap').find('.uagb-faq-item').addClass('uagb-faq-item-active');
-			
-// 			$( document ).delegate( ".uagb-faq-layout-accordion .uagb-faq-questions-button", "click", UAGBFaq.faqButtonClick )
-// 		},
-// 		/**
-// 		 * Smooth Scroll.
-// 		 */
-// 		faqButtonClick: function( e ) {
-
-//             var faqItem = $( this ).parent();
-            
-//             if ( faqItem.hasClass('uagb-faq-item-active') ) {
-
-//                 faqItem.removeClass('uagb-faq-item-active');
-//                 faqItem.find('.uagb-content').slideUp( 'normal');
-//             } else {
-
-//                 faqItem.addClass('uagb-faq-item-active');
-//                 faqItem.find('.uagb-content').slideDown( 'normal');
-//             }
-// 		},
-// 	}
-
-// 	$( document ).ready(function() {
-// 		UAGBFaq.init()
-// 	})
-
-
-// } )( jQuery ) 
+}
